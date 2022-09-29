86163d59e230ec194a513f3624d26b60ea2caa0e
gafouni <haouabouna@gmail.com>
2022-09-29 14:49:26 +0200

    data fixtures tricksFictures

--- src/DataFixtures/TricksFixtures.php
+++ src/DataFixtures/TricksFixtures.php
@@ -4,36 +4,31 @@ namespace App\DataFixtures;
 
 use Faker;
 use App\Entity\User;
+use App\Entity\Trick;
+use App\Entity\Message;
+use App\Entity\TrickGroup;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class TricksFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+        {
+            $this->encoder = $encoder;
+        }
 
     public function load(ObjectManager $manager): void
     {
         //Utilisation de Faker
         $faker = Faker\Factory::create('fr_FR');
 
-        //Creation des categories (trickGroup)//
-        for($j=0; $j<=4; $j++){
-
-            $trickGroup = new TrickGroup();
-
-            $trickGroup->setName($faker->name)
-                        ->setDescription($faker->description)
-                        ->setSlug($faker->slug);
-            
-            $manager->persist(trickGroup);   
-            
-                //Creation des 
-
-        }
-
 
         //Creation des utilisateurs//
-        for($i = 1; $i<=15; $i++){
+        for($i = 1; $i<=12; $i++){
             // $user = new UserPasswordEncoderInterface();
             // $user->setPassword($this->encoder->encodePassword($user, 'gafouni'));
 
@@ -54,10 +49,54 @@ class TricksFixtures extends Fixture
             $user->setIsVerified($faker->numberBetween(0,1));
 
             $manager->persist($user);
-        }     
-        
-        // $product = new Product();
-        // $manager->persist($product);
+
+        }    
+
+        //Creation des categories (trickGroup)//
+        for($j=0; $j<=3; $j++){
+
+            $trickGroup = new TrickGroup();
+
+            $trickGroup->setName($faker->word)
+                        ->setDescription($faker->words(10, true))
+                        ->setSlug($faker->slug());
+            
+            $manager->persist($trickGroup);   
+            
+            //Creation des tricks (5 trikcs maximum par trickGroups)//
+            for($k=0; $k<=4; $k++){
+                // $user = $this->getReference('user'. $faker->numberBetween(1,20));
+
+                $trick = new Trick();
+
+                $trick->setTrickName($faker->words(6, true))
+                        ->setDescription($faker->text())
+                        ->setImgFile('/assets/img/ImagesBD/manipulation-g67b941e5c_1920.jpg')
+                        ->setVideoFile('/assets/img/ImagesBD/manipulation-g67b941e5c_1920.jpg')
+                        ->setSlug($faker->slug())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 month', 'now'))
+                        ->setTrickGroup($trickGroup)
+                        ->setUser($user);
+                        
+
+                $manager->persist($trick);  
+                
+                    //Creation des messages: maximum 15 messages par annonce//
+                    for($h=0; $h<=14; $h++){
+                        $message = new Message();
+
+                        $message->setContent($faker->text())
+                                ->setCreatedAd($faker->datetimeBetween('-6 month', 'now'))
+                                ->setTrick($trick)
+                                ->setUser($user);
+
+                        $manager->persist($message);  
+
+
+                    }
+            }
+
+        }
 
         $manager->flush();
     }
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -1,54 +1,56 @@
 <?php
 
-namespace App\DataFixtures;
+// namespace App\DataFixtures;
 
-use Faker;
-use App\Entity\User;
-use Doctrine\Persistence\ObjectManager;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+// use Faker;
+// use App\Entity\User;
+// use Doctrine\Persistence\ObjectManager;
+// use Doctrine\Bundle\FixturesBundle\Fixture;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UsersFixtures extends Fixture
+// class UsersFixtures extends Fixture
 {
-    private $encoder;
+    // private $encoder;
 
-    public function __construct(UserPasswordEncoderInterface $encoder)
-        {
-            $this->encoder = $encoder;
-        }
+    // public function __construct(UserPasswordEncoderInterface $encoder)
+    //     {
+    //         $this->encoder = $encoder;
+    //     }
 
-    public function load(ObjectManager $manager): void
-    {
-        //Utilisation de Faker
-        $faker = Faker\Factory::create('fr_FR');
+    // public function load(ObjectManager $manager): void
+    // {
+    //     //Utilisation de Faker
+    //     $faker = Faker\Factory::create('fr_FR');
 
-        //Creation des utilisateurs//
-        for($i = 1; $i<=15; $i++){
-            // $user = new UserPasswordEncoderInterface();
-            // $user->setPassword($this->encoder->encodePassword($user, 'gafouni'));
+    //     //Creation des utilisateurs//
+    //     for($i = 1; $i<=10; $i++){
+    //         // $user = new UserPasswordEncoderInterface();
+    //         // $user->setPassword($this->encoder->encodePassword($user, 'gafouni'));
 
-            $user = new User();
-            $user->setEmail($faker->email);
-            $user->setPassword('motdepasse');
+    //         $user = new User();
+    //         $user->setEmail($faker->email);
+    //         $user->setPassword('motdepasse');
             
-            if($i === 1)
-                $user->setRoles(['ROLE_ADMIN']);
-            else
-                $user->setRoles(['ROLE_USER']);    
+    //         if($i === 1)
+    //             $user->setRoles(['ROLE_ADMIN']);
+    //         else
+    //             $user->setRoles(['ROLE_USER']);    
        
-            // $password = $this->encoder->encodePassword($user, 'password');
-            // $user->setPassword($password);
-            $user->setFirstName($faker->firstName);
-            $user->setLastName($faker->lastName);
-            $user->setPictureFile('maphoto');
-            $user->setIsVerified($faker->numberBetween(0,1));
+    //         // $password = $this->encoder->encodePassword($user, 'password');
+    //         // $user->setPassword($password);
+    //         $user->setFirstName($faker->firstName);
+    //         $user->setLastName($faker->lastName);
+    //         $user->setPictureFile('maphoto');
+    //         $user->setIsVerified($faker->numberBetween(0,1));
 
-            $manager->persist($user);
-        }     
+    //         $manager->persist($user);
+
+    //         //On enregistre l'utilisateur dans une reference//
+    //         $this->setReference('user' .$i, $user);
+    //     }     
         
-        // $product = new Product();
-        // $manager->persist($product);
+        
 
-        $manager->flush();
-    }
+    //     $manager->flush();
+    // }
 }
